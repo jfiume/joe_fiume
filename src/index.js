e1f17342b1b0fdf476de5316ddc86435d982a33b
Joe Fiume <joefiume88@gmail.com>
2018-08-06 15:29:57 -0700

    added a store and root reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+// comment out when not debugging
+import logger from "redux-logger";
+import RootReducer from './reducers/root_reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(RootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/root_reducer.js
@@ -0,0 +1,7 @@
+// import { combineReducers } from 'redux';
+
+const RootReducer = (state = {}, action) => {
+  return state;
+};
+
+export default RootReducer;
