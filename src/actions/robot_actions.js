f1b9400a9ff8d46e8b8e4049f8d8693b29108740
Joe Fiume <joefiume88@gmail.com>
2018-08-07 17:07:08 -0700

    added a comment on async calls

--- src/actions/robot_actions.js
+++ src/actions/robot_actions.js
@@ -7,6 +7,7 @@ export const receiveRobot = robot => ({
 
 export const fetchRobot = (text) => dispatch => {
   // normally I use a async request here then a promise. However, the robot loads synchronously from its api.
+  // for the typically async calls I like to use a try, catch block
   const url = text.text;
   const robot = {"robot":`https://robohash.org/${url}.png`}
   return (
